f51dfc82f056a9bf060a4bf859f43d51c6653d86
Aldebarantech <Aldebarantech@DESKTOP-5N2DJ1L>
2016-08-10 17:24:39 -0500

    Version 2.0 ejercicio 18

--- src/ejercicio18l1/Ejercicio18L1.java
+++ src/ejercicio18l1/Ejercicio18L1.java
@@ -16,8 +16,11 @@ public class Ejercicio18L1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Creamos un array de objetos de la clase empleados
+        int numuser=0,busqueda=0;
+        Telefonos arrayObjetos[]=new Telefonos[10];
         String nombre,apellido1,apellido2;
-        int option;
+        int option=10;
         Scanner lector;
         lector = new Scanner(System.in);
         System.out.println("\nDigite Nombre");
@@ -26,32 +29,64 @@ public class Ejercicio18L1 {
         apellido1=lector.nextLine();
         System.out.println("Digite Segundo Apellido");
         apellido2=lector.nextLine();
-        Telefonos cliente1;
-        cliente1= new Telefonos(nombre,apellido1,apellido2);
-    do{
+        arrayObjetos[numuser]= new Telefonos(nombre,apellido1,apellido2);
+    do{ 
         System.out.println("\nDigite Una Opcion ");
-        System.out.println("1. Mostrar Nombre");
-        System.out.println("2. Modificar nombre");
-        System.out.println("3. Modificar Telefono");
-        System.out.println("4. Mostrar Nombre y telefono");
+        System.out.println("1. Ingresar Cliente");
+        System.out.println("2. Buscar cliente");
         System.out.println("0. Salir\n");
         option=lector.nextInt();
         
         switch (option){
-            case 1:cliente1.mostrar_nombre();
-                   break;
-            case 2: cliente1.setNombre(lector.next());
+            case 1: numuser++;
+                    System.out.println("\nDigite Nombre");
+                    nombre=lector.next();
+                    System.out.println("Digite Apellido");
+                    apellido1=lector.next();
+                    System.out.println("Digite Segundo Apellido");
+                    apellido2=lector.next();
+                    System.out.println("\nDigite un nuevo usuario ");
+                    arrayObjetos[numuser]= new Telefonos(nombre,apellido1,apellido2);
+                    break;
+            case 2: System.out.println("\nDigite nombre cliente ");
+                    nombre=lector.next();
+                    for(int i=0;i<arrayObjetos.length;i++){
+                        if(nombre.equals(arrayObjetos[i].getNombre())){
+                            busqueda=i;
+                            break;
+                        }else
+                            System.out.println("\nNombre erroneo ");
+                            
+                            
+                    }
+                    do{
+                    System.out.println("\nDigite Una Opcion ");
+                    System.out.println("1. Mostrar Nombre");
+                    System.out.println("2. Modificar nombre");
+                    System.out.println("3. Modificar Telefono");
+                    System.out.println("4. Mostrar Nombre y telefono");
+                    System.out.println("9. Salir\n");
+                    option=lector.nextInt();
+        
+                    switch (option){
+                        case 1: arrayObjetos[busqueda].mostrar_nombre();
+                                break;
+                        case 2: arrayObjetos[busqueda].setNombre(lector.next());
+                                break;
+                        case 3: arrayObjetos[busqueda].setTelefono(lector.nextInt());
+                                break;
+                        case 4:
+                                arrayObjetos[busqueda].mostrar_nombre();
+                                arrayObjetos[busqueda].mostrar_telefono();
+                                break;                     
+                    }
+                    }while(option!=9);
                 break;
-            case 3: cliente1.setTelefono(lector.nextInt());
-                break;
-            case 4:
-                cliente1.mostrar_nombre();
-                cliente1.mostrar_telefono();
-                break;
-                
-                             
         }
-    }while(option!=0);
+        
+        }while(option!=0);
+        
+        
     }
     
 }
